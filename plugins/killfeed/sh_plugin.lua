4cca6b037ef456c4bc980871b5210022b4fd99d0
PatriotRossii <rorkh@internet.ru>
2021-04-21 22:33:26 +0500

    Mistake fix

--- plugins/killfeed/sh_plugin.lua
+++ plugins/killfeed/sh_plugin.lua
@@ -4,6 +4,9 @@ PLUGIN.name = "Kill Feed"
 PLUGIN.author = "Rorkh"
 PLUGIN.description = "Kill feed."
 
+ix.util.Include("sv_plugin.lua")
+ix.util.Include("cl_plugin.lua")
+
 -- LANGUAGE
 if (CLIENT) then
 	do
