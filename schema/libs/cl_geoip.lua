da25501d268519904ff744b6fa05f946a56c1f6b
PatriotRossii <rorkh@internet.ru>
2021-04-21 15:52:47 +0500

    Geoip specific optimisation

--- schema/libs/cl_geoip.lua
+++ schema/libs/cl_geoip.lua
@@ -2,7 +2,14 @@ ix.geoip = ix.geoip or {}
 ix.geoip.cache = ix.geoip.cache or {}
 
 hook.Add("CharacterLoaded", "GeoIP", function()
+	local LP = LocalPlayer()
+
 	for _, v in ipairs(player.GetHumans()) do
+		if v == LP then
+			-- TODO: Country codes map for other languages
+			ix.option.Set("language", client:GetNetVar("country_code") == "ru" and "russian" or "english", true)
+		end
+
 		if (IsValid(v)) then
 			ix.geoip:GetMaterial(v)
 		end
