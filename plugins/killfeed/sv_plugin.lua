baaefd63962eff387b9a6a4aad96f45c870f41e9
PatriotRossii <rorkh@internet.ru>
2021-04-23 19:14:46 +0500

    KillFeed v1

--- plugins/killfeed/sv_plugin.lua
+++ plugins/killfeed/sv_plugin.lua
@@ -3,9 +3,14 @@ util.AddNetworkString("killfeed.Message")
 local PLUGIN = PLUGIN
 
 function PLUGIN:DoPlayerDeath(player, attacker, dmg)
+	local weapon = attacker.GetActiveWeapon and attacker:GetActiveWeapon() or nil
+	local attacker = attacker:GetName()
+
 	net.Start("killfeed.Message")
-		net.WriteUInt(player:UserID(), 8)
-		net.WriteUInt(attacker:IsPlayer() and attacker:UserID() or 0, 8)
+		net.WriteString(player:GetName())
+		net.WriteString(attacker)
+		net.WriteString(weapon and weapon:GetName() or "NULL")
+
 		net.WriteBool(player:GetNetVar("bleeding", false))
 	net.Broadcast()
 end
